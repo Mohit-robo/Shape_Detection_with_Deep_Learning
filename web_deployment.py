d60cc5dfae9b2550aec8025b6e32b4ab63f24085
Mohit Gaikwad <82194525+Mohit-robo@users.noreply.github.com>
2021-11-08 13:50:48 +0530

    Add files via upload

--- web_deployment.py
+++ web_deployment.py
@@ -1,22 +1,26 @@
+### Import librariers ###
+
 from random import random
 import streamlit as st
 import tensorflow as tf
 import streamlit as st
 import cv2
 
-st.set_page_config(page_title = "Shape Classifier",page_icon="♦")
+st.set_page_config(page_title = "Shape Classifier",page_icon="♦")       ### Set web page title
 
 @st.cache(allow_output_mutation=True)
+
+### Load trained model ###
+
 def load_model():
   model=tf.keras.models.load_model(r'C:\Users\PUsH\Desktop 1\Shape_rec\my_model.h5')
   return model
 with st.spinner('Model is being loaded..'):
   model=load_model()
 
-st.write("""
-         # Shape Classification
-         """
-         )
+st.title("Shape Classifier")
+ 
+### Image preprocessing before feeding the model to predict
 
 def grayscale(img):
     img = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
@@ -30,29 +34,37 @@ def preprocessing(img):
     img = img/255
     return img
 
-file = st.file_uploader("Please upload an brain scan file", type=["jpg", "png"])
+### File uploader section of the webapp ###
+
+file = st.file_uploader("Please upload an brain scan file", type=["jpg", "png","jpeg"])
 
 from PIL import Image, ImageOps
 import numpy as np
+import time
 st.set_option('deprecation.showfileUploaderEncoding', False)
+
+### Creating the predition function ###
+
 def import_and_predict(image_data, model):
     
-        size = (200,200)    
+        size = (200,200)     ### *** Mention the size of the image that the model was trained on  
         image = ImageOps.fit(image_data, size, Image.ANTIALIAS)
         image = np.asarray(image)
         img = cv2.resize(image,(200,200))
         img = preprocessing(img)
         img = img.reshape(1, 200,200, 1)
-        prediction = model.predict(img)    
+        prediction = model.predict(img)       ### Apply the model to the testing image
         return prediction
 
 if file is None:
-    st.text("Please upload an image file")
+    st.caption("Please upload an image file")   ### Starts with this comment
 else:
     image = Image.open(file)
-    st.image(image, use_column_width=True)
+    st.image(image, use_column_width=True)     ### shows the image on the display  
     predictions = import_and_predict(image, model)
     classNo = np.argmax(predictions,axis=1)
+
+    ### The classes to be predicted
 
     if   classNo[0] == 0: label = 'circle'
     elif classNo[0] == 1: label =  'square'
@@ -60,8 +72,13 @@ else:
     elif classNo[0] == 3: label =  'triangle'
 
     score = tf.nn.softmax(predictions[0])
+
+    with st.spinner('Result Loading....'):
+        time.sleep(2)
+    
     st.success('Detected:{}'.format(label))
-    st.write(classNo)
+    # st.write(classNo)
+    
     print(
     "This image most likely belongs to {} with a {:.2f} percent confidence."
     .format(label, 100 * np.max(score))
